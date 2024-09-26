f0807e77c1411ac163179fb113a03e581a819bfc
Youzhou Zhang <czhang@lcs.on.ca>
2024-09-26 11:52:42 -0400

    finished test for calculation

--- ShiftySumsTest/ShiftySumsTest.swift
+++ ShiftySumsTest/ShiftySumsTest.swift
@@ -9,8 +9,16 @@ import Testing
 
 struct ShiftySumsTest {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test func twelveShiftThree()  {
+        #expect(shiftTheNumber(number: 12, shift: 3) == 13332)
     }
-
+    @Test func eighteenShiftFive()  {
+        #expect(shiftTheNumber(number: 18, shift: 5) == 1999998)
+    }
+    @Test func hundredShiftZero()  {
+        #expect(shiftTheNumber(number: 100, shift: 0) == 100)
+    }
+    @Test func N1939Shift5()  {
+        #expect(shiftTheNumber(number: 1939, shift: 5) == 215444229)
+    }
 }
